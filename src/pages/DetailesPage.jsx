edc738c3ef5ba82ca9f25cfe6317f5d7364c2812
FaribaTavakoli2001 <117892836+FaribaTavakoli2001@users.noreply.github.com>
2024-08-05 19:04:42 +0330

    getdetaliespost

--- src/pages/DetailesPage.jsx
+++ src/pages/DetailesPage.jsx
@@ -6,16 +6,16 @@ import { getDetailesPost } from '../services/user'
 function DetailesPage() {
     const { id } = useParams()
     // console.log(id)
-  
-    const { data , refetch } = useQuery({
-        queryKey: ['get-details-post' ],
-        queryFn:getDetailesPost ,
-        enabled: !id
-      })
 
-      const filteredData = data?.filter(item => item.id);
-      console.log(filteredData)
- 
+  
+    const { data , isPending } = useQuery({
+        queryKey: ['get-details-post',id],
+        queryFn:() => getDetailesPost(id) ,
+        // enabled: !id
+      })
+      console.log({data, isPending})
+
+  
 
   return (
     <div>DetailesPage : {id}</div>
